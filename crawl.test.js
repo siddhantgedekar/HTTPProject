d3e86d028b3f040e53d9601e4ad6b5864330aaa4
siddhantgedekar <sapwoodg18@gmail.com>
2025-07-03 19:05:25 +0530

    normalizeURL and tests

---
+++ crawl.test.js
@@ -0,0 +1,31 @@
+const {normalizeURL} = require('./crawl.js')
+const { test, expect } = require('@jest/globals')
+
+test('normalizeURL strip protocol', () => {
+    // thinking input
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+test('normalizeURL strip trailing slash', () => {
+    // thinking input
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+test('normalizeURL capitals', () => {
+    // thinking input
+    const input = 'https://BLOG.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+test('normalizeURL strip http', () => {
+    // thinking input
+    const input = 'http://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
